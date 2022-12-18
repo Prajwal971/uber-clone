42349ecdefad5ff3a79670c6c5162b143bc69748
prajwal971 <prajwalkumar971@gmail.com>
2022-12-18 15:32:19 +0400

    Navigation added.

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,8 @@
 import { FlatList, Image, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
+import { Icon } from '@rneui/themed';
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
     {
@@ -18,6 +20,7 @@ const data = [
 ]
 
 const NavOptions = () => {
+    const navigation = useNavigation();
     return (
         <FlatList
             data={data}
@@ -25,6 +28,7 @@ const NavOptions = () => {
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
                 <TouchableOpacity
+                    onPress={() => navigation.navigate(item.screen)}
                     style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
                 >
                     <View>
@@ -36,6 +40,12 @@ const NavOptions = () => {
                         />
                     </View>
                     <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                    <Icon
+                        style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                        name='arrowright'
+                        color="white"
+                        type="antdesign"
+                    />
                 </TouchableOpacity>
             )}
         />
