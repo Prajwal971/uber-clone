f2508246605418ec9d6ad43ecd5fb322704e4d90
prajwal971 <prajwalkumar971@gmail.com>
2022-12-19 14:15:44 +0400

    Maps implemented with input condition.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,11 @@ import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { selectOrigin, selectDestination, setOrigin, setDestination } from "../slices/navSlice";
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
     return (
         <SafeAreaView style={tw`mt-5 bg-white h-full`}>
             <View style={tw`p-5`}>
@@ -26,6 +29,17 @@ const HomeScreen = () => {
                             fontSize: 18
                         }
                     }}
+                    onPress={(data, details = null) => {
+                        dispatch(setOrigin({
+                            location: details.geometry.location,
+                            description:data.description
+                        }))
+                        dispatch(setDestination(null))
+                    }}
+                    fetchDetails={true}
+                    returKeyType={"search"}
+                    enablePoweredByContainer={false}
+                    minLength={2}
                     query={{
                         key: GOOGLE_MAPS_APIKEY,
                         language: 'en'
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,12 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import tw from 'tailwind-react-native-classnames';
+import Map from '../components/Map';
 
 const MapScreen = () => {
-  return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
-  )
+    return (
+        <View>
+            {/* Map screen occupied 1/2 screen */}
+            <View style={tw`h-1/2`}>
+                <Map />
+            </View>
+            <View style={tw`h-1/2`}></View>
+        </View>
+    )
 }
 
 export default MapScreen
