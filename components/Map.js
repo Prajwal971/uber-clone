9d9852141eab100117aad299add386c08c7733c7
prajwal971 <prajwalkumar971@gmail.com>
2022-12-20 15:34:46 +0400

    fitToSuppliedMarkers added

--- components/Map.js
+++ components/Map.js
@@ -16,7 +16,9 @@ const Map = () => {
         if (!origin || !destination) return;
 
         // Zoom and fit to the markers
-        mapRef.current.fitToSuppliedMarkers(['origin', 'destination'])
+        mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+            edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }
+        })
     }, [origin, destination])
     return (
         <MapView
