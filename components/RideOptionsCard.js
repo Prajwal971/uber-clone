4a032b4b20d8339241a4f67ab17372a9c36bf1cd
prajwal971 <prajwalkumar971@gmail.com>
2022-12-21 11:27:40 +0400

    added internation price,time,miles, selected index ,greyOut button.

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,12 +1,84 @@
-import { SafeAreaView, Text } from 'react-native'
-import React from 'react'
+import { FlatList, SafeAreaView, Text, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import tw from 'tailwind-react-native-classnames';
+import { Icon } from '@rneui/themed';
+import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectTravelTimeInformation } from '../slices/navSlice';
+
+const data = [
+    {
+        id: "Uber-X-123",
+        title: "Uber X",
+        multiplier: 1,
+        image: "https://links.papareact.com/3pn",
+    },
+    {
+        id: "Uber-X-456",
+        title: "Uber XL",
+        multiplier: 1.2,
+        image: "https://links.papareact.com/5w8",
+    },
+    {
+        id: "Uber-X-789",
+        title: "Uber LUX",
+        multiplier: 1.75,
+        image: "https://links.papareact.com/7pf",
+    },
+]
+const SUR_CHARGE_RATE = 1.5;
 
 const RideOptionsCard = () => {
-  return (
-    <SafeAreaView>
-      <Text>Select a ride</Text>
-    </SafeAreaView>
-  )
+    const navigation = useNavigation();
+    const [selected, setSelected] = useState(null)
+    const travelTimeInformation = useSelector(selectTravelTimeInformation);
+    return (
+        <SafeAreaView style={tw`bg-white flex-grow `}>
+            <View>
+                <TouchableOpacity onPress={() => navigation.navigate('NavigateCard')} style={tw`absolute top-3 left-5 p-3 rounded-full`}>
+                    <Icon name="chevron-left" type='fontawesome' />
+                </TouchableOpacity>
+                <Text style={tw`text-center py-5 text-xl`}>Select a ride - {travelTimeInformation?.distance.text}</Text>
+            </View>
+            <FlatList
+                data={data}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item: { id, title, multiplier, image }, item }) => (
+                    <TouchableOpacity onPress={() => { setSelected(item) }} style={tw`flex-row item-center justify-center px-10 ${id === selected?.id && "bg-gray-20"}`}>
+                        <Image
+                            style={{
+                                width: 100,
+                                height: 100,
+                                resizeMode: "contain"
+                            }}
+                            source={{ uri: image }}
+                        />
+                        <View style={tw`-ml-6`}>
+                            <Text style={tw`text-xl font-semibold`}>{title}</Text>
+                            <Text>{travelTimeInformation?.duration.text} Travel Time</Text>
+                        </View>
+                        <Text style={tw`text-xl`}>
+                            {
+                                new Intl.NumberFormat('en-gb', {
+                                    style: 'currency',
+                                    currency: 'INR'
+                                }).format(
+                                    (travelTimeInformation?.duration.value * SUR_CHARGE_RATE * multiplier) / 100
+                                )
+                            }
+                        </Text>
+                    </TouchableOpacity>
+                )}
+            />
+            <View>
+                <TouchableOpacity
+                    disabled={!selected}
+                    style={tw`bg-black py-3 m-3 ${selected && 'bg-gray-300'}`}>
+                    <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    )
 }
 
 export default RideOptionsCard
